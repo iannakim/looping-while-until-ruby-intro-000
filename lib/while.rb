66c0c00abb3393650bcf3043a593b1c2eaf8e247
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-03-25 16:21:30 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,9 @@
 def using_while(levitation_force)
   levitation_force = 6
-loop do
   while levitation_force < 10
+  loop do
     puts "Wingardium Leviosa"
+  end
     levitation_force += 1
   end
 end
